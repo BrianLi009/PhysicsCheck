5dacc94f4258388856177f2c7dd06946c53487b6
Brian Li <brianli.li@mail.utoronto.ca>
2022-07-12 19:35:54 -0400

    the joining and solving of -s not implemented in pipeline yet, if user run it, it will cube and terminate without solving

--- 3-cube-merge-solve.sh
+++ 3-cube-merge-solve.sh
@@ -24,12 +24,16 @@ if [ "$c" -eq 1 ] && [ "$p" -eq 1 ]
 then
      echo "both -s, -p enabled"
     ./gen_cubes/cube.sh -p -s $n $f $r #cube till r varaibles are eliminated
+    echo "solving option for -s not yet implemented"
+    exit 0
 fi
 
 if [ "$c" -eq 1 ] && [ "$p" -eq 0 ]
 then
      echo "-s enabled"
     ./gen_cubes/cube.sh -s $n $f $r #cube till r varaibles are eliminated
+    echo "solving option for -s not yet implemented"
+    exit 0
 fi
 
 if [ "$c" -eq 0 ] && [ "$p" -eq 1 ]
@@ -44,7 +48,7 @@ then
     ./gen_cubes/cube.sh $n $f $r #cube till r varaibles are eliminated
 fi
 
-: '
+:
 #find the deepest cube file
 cube_file=$(find . -type f -wholename "./$n-cubes/*.cubes" -exec grep -H -c '[^[:space:]]' {} \; | sort -nr -t":" -k2 | awk -F: '{print $1; exit;}')
 
@@ -60,6 +64,6 @@ do
     #join the cube to the instance, simplified until 50% of the variables are eliminated
     ./maplesat-ks/simp/maplesat_static $cube_file$i.adj.simp -no-pre -exhaustive=$n.exhaust -order=$n
 done
-'
+
 
 #this is still in the work, when -s option enabled, the script should be different.
