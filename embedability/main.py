e042b73ff72d87eda17a6a62aa621e3df755c362
BrianLi009 <brianli.li@mail.utoronto.ca>
2022-09-15 14:14:39 -0400

    separate verification from checking

--- embedability/main.py
+++ embedability/main.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 from networkx.algorithms.isomorphism.isomorph import is_isomorphic
 from networkx.generators.classic import cycle_graph
 from helper import cross, dot, nested_cross
+from verify_sat import verify_sat
 
 import sys, getopt
 
@@ -277,36 +278,15 @@ def determine_embed(g, assignment, g_sat, order, index, using_subgraph, normaliz
     io.write('        f.write(g_sat + "\\n") \n')
     io.write('if result == sat: \n')
     io.write('    with open(output_sat_f, "a+") as f: \n')
-    io.write('        f.write(g_sat + "\\n") \n')
+    io.write('        f.write(g_sat + "\\n")')
     if verify:
-        io.write('    m = s.model() \n')
-        io.write('    with open(output_sat_f, "a+") as f: \n')
-        for i in assignment.assign:
-            io.write("        f.write( " + " '" + "vertex " + str(i) + ":' + '\\n' ) " + "\n")
-            io.write('        f.write(str(m.evaluate(ver' + str(i) + '[0])) + "\\n" )' + '\n')
-            io.write('        f.write(str(m.evaluate(ver' + str(i) + '[1])) + "\\n" )' + '\n')
-            io.write('        f.write(str(m.evaluate(ver' + str(i) + '[2])) + "\\n" )' + '\n')
-        io.write('    check = Solver()\n')
-        for v in assignment.assign:
-            for v2 in assignment.assign:
-                if v2 in g[v]:
-                    str_format = "    check.add(m.evaluate(ver{0}[0] * ver{1}[0]+ver{0}[1] * ver{1}[1]+ver{0}[2] * ver{1}[2] == 0))\n"
-                    io.write(str_format.format(str(v), str(v2)))
-                #check noncolinear
-                if v2 != v:
-                    str_format = "    check.add(Not(And(m.evaluate(ver{0}[1] * ver{1}[2] - ver{0}[2]* ver{1}[1] == 0), m.evaluate(ver{0}[2] * ver{1}[0] - ver{0}[0]* ver{1}[2] == 0), m.evaluate(ver{0}[0] * ver{1}[1] - ver{0}[1]* ver{1}[0] == 0)))) \n"
-                    io.write(str_format.format(str(v), str(v2)))
-        io.write('    with open(output_sat_f, "a+") as f: \n')
-        io.write('        if check.check() == unsat:\n')
-        io.write('            f.write("verification failed") \n')
-        io.write('        if check.check() == sat:\n')
-        io.write('            f.write("verified")\n')
-        io.write('        else:\n')
-        io.write('            f.write("error during verification")')
-    #with open('file.py', mode='w') as f:
-    #    print(io.getvalue(), file=f)
-    exec (io.getvalue())
-    io.close()
+        with open('file.py', mode='w') as f:
+            print(io.getvalue(), file=f)
+        verify_sat('file.py', assignment, g, output_unsat_f, output_sat_f)
+        exec(open('file.py').read())
+    else:
+        exec (io.getvalue())
+        io.close()
 
 #graph in sat labeling format
 
