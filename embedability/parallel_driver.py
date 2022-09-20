010e48852af6fe1413e6a4c448c7931f018af1b4
BrianLi009 <brianli.li@mail.utoronto.ca>
2022-09-20 18:50:00 -0500

    Add files via upload

---
+++ embedability/parallel_driver.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from subprocess import call
+import time
+def solve_line(n, line_index, file_to_solve):
+    line_index = int(line_index)
+    n = int(n)
+    f = open(file_to_solve)
+    lines = f.readlines()
+    line = lines[n]
+    start_time = time.time()
+    call(['python', 'main.py', str(line), n, "0", "0", "0", "test_unsat.txt", "test_sat.txt", "0", "0"])
+    runtime = time.time() - start_time
+    file_object = open('runtime_' + str(order) + '.txt', 'a')
+    file_object.write(str(n) + ": " + str(runtime) + "\n")
+    # Close the file
+    file_object.close()
+    
+if __name__ == "__main__":
+	solve_by_line(sys.argv[1], sys.argv[2], sys.argv[3])
