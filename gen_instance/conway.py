0cbcb79db493d9683a3b9b99a2f91c06cecd6513
BrianLi009 <brianli.li@mail.utoronto.ca>
2023-01-03 02:25:18 -0500

    testing the conway constraint

--- gen_instance/conway.py
+++ gen_instance/conway.py
@@ -2,13 +2,16 @@ import itertools
 
 def conway(n, edge_dict, tri_dict, v, t, cnf):
     #vertex v is connected to at least t triangles
+    clause_count = 0
+    cnf_file = open(cnf, 'a+')
     vertices_lst = list(range(1, n+1))
     all_tri = list(itertools.combinations(vertices_lst, 3))
     v_tri_lst = [tri for tri in all_tri if v in tri]
     tr_sublst = itertools.combinations(v_tri_lst, int(len(v_tri_lst)-t))
     for comb in tr_sublst:
-        clause = []
+        clause = ""
         for tri in comb:
-            clause.append(tri_dict[tri])
-        cnf.append(clause)
-    return cnf
+            clause = clause + str(tri_dict[tri]) + " "
+        cnf_file.write(clause + " 0\n")
+        clause_count += 1
+    return clause_count
