cc851f46aaeaf6e38252d5c2c51124688dfbe440
Curtis Bright <cbright@gmail.com>
2023-05-03 01:12:19 -0400

    Simplify the generation of the embeddability constraints - generate them directly, not via a string passed to exec

--- embedability/helper.py
+++ embedability/helper.py
@@ -16,7 +16,7 @@ def cross(v,w):
 #define new variable, pass 
 
 def dot(v,w):
-    return (v+'[0]'+'*'+w+'[0]' + '+' + v+'[1]'+'*'+w+'[1]' + '+' + v +'[2]'+'*'+w+'[2]')
+    return (v[0]*w[0] + v[1]*w[1] + v[2]*w[2])
 
 def nested_crossc(x):
     if isinstance(x, tuple):
--- embedability/main.py
+++ embedability/main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python
-from io import StringIO
 from networkx.algorithms import isomorphism
-import os
-import csv
 import networkx as nx
 import collections
-import itertools
-from collections import Counter
 from collections import defaultdict
 
-from networkx.algorithms.isomorphism.isomorph import is_isomorphic
-from networkx.generators.classic import cycle_graph
-from helper import cross, dot, nested_cross
+from helper import cross, dot
 from verify_sat import verify_sat
+from z3 import *
 
-import sys, getopt
+import sys
 
 """
 find_assignments take in a graph and find all possible interpretations of its orthogonal relations.
@@ -173,6 +167,14 @@ def find_assignments(g):
     completed.sort(key=lambda f: (f.nvar, len(f.ortho)))
     return completed
 
+# Return the constraint that c = ±(a × b)
+def cross_constraint(a, b, c):
+    return Or(And(c[0]==cross(a,b)[0], c[1]==cross(a,b)[1], c[2]==cross(a,b)[2]), And(c[0]==cross(b,a)[0], c[1]==cross(b,a)[1], c[2]==cross(b,a)[2]))
+
+# Return the constraint that a is not the zero vector
+def not_zero(a):
+    return Or(a[0]!=0, a[1]!=0, a[2]!=0)
+
 def determine_embed(g, assignment, g_sat, order, index, using_subgraph, normalize, output_unsat_f, output_sat_f, prop1, verify):
     """
     g: dictonary of vertices and their adjacent vertices
@@ -189,50 +191,38 @@ def determine_embed(g, assignment, g_sat, order, index, using_subgraph, normaliz
     """
     #print (assignment)
     #print (g)
-    io = StringIO()
-    io.write('from helper import cross \n')
-    io.write('from z3 import * \n')
-    io.write("s = Solver()\n")
-    v_dict = {}
+
+    s = Solver()
+    v = {}
+    ver = {}
     for i in range(len(assignment.var)):
-        v_dict[i] = ('v'+str(i)+'c1', 'v'+str(i)+'c2', 'v'+str(i)+'c3') #{0: (v0c1, v0c2, v0c3)}
+        v[i] = (Real("v{0}c1".format(i)), Real("v{0}c2".format(i)), Real("v{0}c3".format(i))) #{0: (v0c1, v0c2, v0c3)}
     for i in range(len(assignment.var)):
-        #first define the base vectors
-        io.write( 'v'+str(i)+'c1 = Real("v'+ str(i) + 'c1")\n')
-        io.write( 'v'+str(i)+'c2 = Real("v'+ str(i) + 'c2")\n')
-        io.write( 'v'+str(i)+'c3 = Real("v'+ str(i) + 'c3")\n')
-        io.write( 'v' + str(i) + '= (' + 'v' + str(i) + 'c1, v' + str(i) + 'c2, v' + str(i) + 'c3)\n')
-        io.write('s.add(' + 'v' + str(i) + 'c3 >= 0)\n')
+        s.add(v[i][2] >= 0)
     #no need to define the rest of the vectors
     for i in assignment.assign:
         #now define every vertex
-        io.write( 'ver'+str(i)+'c1 = Real("ver'+ str(i) + 'c1")\n')
-        io.write( 'ver'+str(i)+'c2 = Real("ver'+ str(i) + 'c2")\n')
-        io.write( 'ver'+str(i)+'c3 = Real("ver'+ str(i) + 'c3")\n')
-        str_format = "ver{0} = (ver{0}c1, ver{0}c2, ver{0}c3)\n"
-        io.write(str_format.format(str(i)))
-        io.write('s.add(' + 'ver' + str(i) + 'c3 >= 0)\n')
-        #io.write('s.add(' + dot('ver' + str(i), 'ver' + str(i)) + '== 1) \n')
+        ver[i] = (Real("ver{0}c1".format(i)), Real("ver{0}c2".format(i)), Real("ver{0}c3".format(i)))
+        s.add(ver[i][2] >= 0)
     for i in assignment.assign:
         #s.add() its corresponding vector as a condition
         if isinstance(assignment.assign[i], int):
-            io.write('s.add('+'ver'+str(i)+'[0]=='+'v'+str(assignment.assign[i])+'[0]) \n')
-            io.write('s.add('+'ver'+str(i)+'[1]=='+'v'+str(assignment.assign[i])+'[1]) \n')
-            io.write('s.add('+'ver'+str(i)+'[2]=='+'v'+str(assignment.assign[i])+'[2]) \n')
+            s.add(ver[i][0]==v[assignment.assign[i]][0])
+            s.add(ver[i][1]==v[assignment.assign[i]][1])
+            s.add(ver[i][2]==v[assignment.assign[i]][2])
             if normalize == "True":
-                io.write('s.add(' + dot('ver' + str(i), 'ver' + str(i)) + '== 1) \n')
+                s.add(dot(ver[i], ver[i]) == 1)
         else:
-            str_format = "s.add(Or(And(ver{2}[0]==cross({0},{1})[0], ver{2}[1]==cross({0},{1})[1], ver{2}[2]==cross({0},{1})[2]), And(ver{2}[0]==cross({1},{0})[0], ver{2}[1]==cross({1},{0})[1], ver{2}[2]==cross({1},{0})[2])))\n"
             if i in assignment.base:
-                io.write(str_format.format("v" + str(assignment.assign[i][0]), "v" + str(assignment.assign[i][1]), str(i)))
+                s.add(cross_constraint(v[assignment.assign[i][0]], v[assignment.assign[i][1]], ver[i]))
             else:
-                io.write(str_format.format("ver" + str(assignment.assign[i][0]), "ver" + str(assignment.assign[i][1]), str(i)))
-    io.write('s.add('+v_dict[0][0] +' == 1) \n')
-    io.write('s.add('+v_dict[0][1] +' == 0) \n')
-    io.write('s.add('+v_dict[0][2] +' == 0) \n')
-    io.write('s.add('+v_dict[1][0] +' == 0) \n')
-    io.write('s.add('+v_dict[1][1] +' == 1) \n')
-    io.write('s.add('+v_dict[1][2] +' == 0) \n')
+                s.add(cross_constraint(ver[assignment.assign[i][0]], ver[assignment.assign[i][1]], ver[i]))
+    s.add(v[0][0] == 1)
+    s.add(v[0][1] == 0)
+    s.add(v[0][2] == 0)
+    s.add(v[1][0] == 0)
+    s.add(v[1][1] == 1)
+    s.add(v[1][2] == 0)
     edges = set()
     for v in g:
         for w in g[v]:
@@ -247,9 +237,7 @@ def determine_embed(g, assignment, g_sat, order, index, using_subgraph, normaliz
             if (v2, v1) in had:
                 continue
             had.add((v1, v2))
-            cross_product = "cross(" + "ver" + str(v1) + "," + "ver" + str(v2) + ")"
-            str_format = "s.add(Or(Not({0}[0]==0), Not({0}[1]==0), Not({0}[2]==0))) \n"
-            io.write(str_format.format(cross_product))
+            s.add(not_zero(cross(ver[v1],ver[v2])))
     #revert assign dict
     assign_inv = defaultdict(list)
     for k, v in assignment.assign.items():
@@ -258,42 +246,40 @@ def determine_embed(g, assignment, g_sat, order, index, using_subgraph, normaliz
         if len(assign_inv[dot_relation[0]]) > 1:
             for v_base in assign_inv[dot_relation[0]]:
                 if v_base in assignment.base:
-                    v = "ver" + str(v_base)
+                    v = v_base
         else:
-            v = "ver" + str(assign_inv[dot_relation[0]][0])
+            v = assign_inv[dot_relation[0]][0]
         if len(assign_inv[dot_relation[1]]) > 1:
             for w_base in assign_inv[dot_relation[1]]:
                 if w_base in assignment.base:
-                    w = "ver" + str(w_base)
+                    w = w_base
         else:
-            w = "ver" + str(assign_inv[dot_relation[1]][0])
-        io.write('s.add(' + dot(v,w) + '== 0) \n')
-    io.write('s.set("timeout", 10000) \n')
-    io.write('result = s.check() \n')
-    io.write('if result == unknown: \n')
-    io.write('    index = int(index) + 1 \n')
-    io.write('    main(g_sat, order, index, using_subgraph, normalize, output_unsat_f, output_sat_f, prop1, verify) \n')
-    io.write('if result == unsat: \n')
-    io.write('    with open(output_unsat_f, "a+") as f: \n')
-    io.write('        f.write(g_sat + "\\n") \n')
-    io.write('if result == sat: \n')
-    io.write('    with open(output_sat_f, "a+") as f: \n')
-    io.write('        f.write(g_sat + "\\n") \n')
-    if verify == "True":
-        io.write('    m = s.model() \n')
-        io.write('    with open("solution.log", "w+") as f2: \n')
-        for i in g:
-            io.write("        f2.write( " + " '" + str(i) + "' + '\\n' ) " + "\n")
-            io.write('        f2.write(str(m.evaluate(ver' + str(i) + '[0]).as_decimal(100)).replace("?","")+' + '"\\n" )' + '\n')
-            io.write('        f2.write(str(m.evaluate(ver' + str(i) + '[1]).as_decimal(100)).replace("?","")+' + '"\\n" )' + '\n')
-            io.write('        f2.write(str(m.evaluate(ver' + str(i) + '[2]).as_decimal(100)).replace("?","")+' + '"\\n" )' + '\n')
-        exec (io.getvalue())
-        io.close()
-        if not verify_sat(g, "solution.log"):
-            print ("verification failed")
-    else:
-        exec (io.getvalue())
-        io.close()
+            w = assign_inv[dot_relation[1]][0]
+        s.add(dot(ver[v],ver[w]) == 0)
+    s.set("timeout", 10000)
+    result = s.check()
+    if result == unknown:
+        # print("Timeout reached: Embeddability unknown")
+        index = int(index) + 1
+        main(g_sat, order, index, using_subgraph, normalize, output_unsat_f, output_sat_f, prop1, verify)
+    if result == unsat:
+        # print("Not embeddable")
+        with open(output_unsat_f, "a+") as f:
+            f.write(g_sat + "\n")
+    if result == sat:
+        # print("Embeddable")
+        with open(output_sat_f, "a+") as f:
+            f.write(g_sat + "\n")
+        if verify == "True":
+            m = s.model()
+            with open("solution.log", "w+") as f2:
+                for i in g:
+                    f2.write(str(i)+"\n")
+                    f2.write(str(m.evaluate(ver[i][0]).as_decimal(100)).replace("?","")+"\n")
+                    f2.write(str(m.evaluate(ver[i][1]).as_decimal(100)).replace("?","")+"\n")
+                    f2.write(str(m.evaluate(ver[i][2]).as_decimal(100)).replace("?","")+"\n")
+            if not verify_sat(g, "solution.log"):
+                print ("verification failed")
 
 #graph in sat labeling format
 
