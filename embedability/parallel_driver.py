3adc735efb82c6a6bb8016fe303bb079b184f389
BrianLi009 <brianli.li@mail.utoronto.ca>
2023-05-04 13:04:12 -0400

    update parallel_driver.py

--- embedability/parallel_driver.py
+++ embedability/parallel_driver.py
@@ -1,20 +1,18 @@
-import sys
-import os
-from subprocess import call
-import time
-def solve_line(n, line_index, file_to_solve):
-    line_index = int(line_index)
-    n = int(n)
-    f = open(file_to_solve)
-    lines = f.readlines()
-    line = lines[n]
-    start_time = time.time()
-    call(['python', 'main.py', str(line), n, "0", "False", "False", "test_unsat.txt", "test_sat.txt", "False", "False"])
-    runtime = time.time() - start_time
-    file_object = open('runtime_' + str(order) + '.txt', 'a')
-    file_object.write(str(n) + ": " + str(runtime) + "\n")
-    # Close the file
-    file_object.close()
-    
-if __name__ == "__main__":
-	solve_by_line(sys.argv[1], sys.argv[2], sys.argv[3])
+from main import main
+from itertools import islice
+
+def solve_range(n, start, end, file_to_solve, using_subgraph, normalize, output_unsat_f, output_sat_f):
+    """
+    example for calling this function:
+    solve_range(19, 2, 6, "embedability/19.exhaust", "False", "False", "unsat_19.out", "sat_19.out")
+    """
+    with open(file_to_solve, 'r') as f:
+    # Skip the lines before start
+        start_line = start
+        for _ in range(start_line):
+            next(f)
+        # Read the next end-start lines
+        num_lines = end-start
+        for line in islice(f, num_lines):
+            print (line)
+            main(line, n, 0, using_subgraph, normalize, output_unsat_f, output_sat_f, prop1="False", verify="False")
