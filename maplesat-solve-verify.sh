7c524c716031c0eecee54f9448816ff4fa1f05f1
BrianLi009 <brianli.li@mail.utoronto.ca>
2023-05-26 01:23:56 -0400

    add learnt clause option

--- maplesat-solve-verify.sh
+++ maplesat-solve-verify.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+while getopts "l" opt
+do
+	case $opt in
+        l) l="-l" ;;
+	esac
+done
+shift $((OPTIND-1))
+
 n=$1 #order
 f=$2 #instance file name
 e=$3 #exhaustive file name
@@ -12,13 +20,20 @@ Usage:
     ./maplesat-solve-verify.sh n f e
 
 Options:
+    [-l]: generate learnt clauses
     <n>: the order of the instance/number of vertices in the graph
     <f>: file name of the CNF instance to be solved
     <e>: file name to output exhaustive SAT solutions
 " && exit
 
+if [ "$l" == "-l" ]
+then
+    echo "MapleSAT will output learnt clause"
+    ./maplesat-ks/simp/maplesat_static $f $f.drat -perm-out=$f.perm -exhaustive=$e -order=$n -no-pre -minclause -unit-out=$f.unit -noncanonical-out=$f.noncanonical| tee $f.log
+else
+    ./maplesat-ks/simp/maplesat_static $f $f.drat -perm-out=$f.perm -exhaustive=$e -order=$n -no-pre -minclause | tee $f.log
+fi
 
-./maplesat-ks/simp/maplesat_static $f $f.drat -perm-out=$f.perm -exhaustive=$e -order=$n -no-pre -minclause | tee $f.log
 # Verify DRAT proof
 ./drat-trim/drat-trim $f $f.drat | tee $f.verify
 if ! grep "s VERIFIED" -q $f.verify
