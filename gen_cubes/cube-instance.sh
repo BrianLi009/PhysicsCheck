48c4c8c53b0330f0f27a883e35719acefe58ea09
Zhengyu Li <brian.li@uwaterloo.ca>
2023-06-21 23:03:55 -0400

    Update cube-instance.sh
    fix verification of UNSAT instance, testing...

--- gen_cubes/cube-instance.sh
+++ gen_cubes/cube-instance.sh
@@ -31,19 +31,9 @@ echo "Processing $cubeline..."
 if [ -z $s ] || [ "$s" == "-m" ] || (( i == 1 ))
 then
 	# Adjoin the literals in the current cube to the instance and simplify the resulting instance with CaDiCaL
-	./gen_cubes/apply.sh $f $dir/$((i-1)).cubes $c > $dir/$((i-1)).cubes$c
-	command="./cadical/build/cadical $dir/$((i-1)).cubes$c $dir/$((i-1)).cubes$c.drat -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
+	command="./gen_cubes/apply.sh $f $dir/$((i-1)).cubes $c | ./cadical/build/cadical -o $dir/$((i-1)).cubes$c.simp -e $dir/$((i-1)).cubes$c.ext -n -c 10000 > $logdir/$((i-1)).cubes$c.simp"
 	echo $command
 	eval $command
-	echo "$dir/$((i-1)).cubes$c is solved by CaDiCaL, thus verifying..."
-	./drat-trim/drat-trim "$dir/$((i-1)).cubes$c" "$dir/$((i-1)).cubes$c.drat" -f | tee log/$((i-1)).cubes$c.verify
-	echo "log/$((i-1)).cubes$c.verify"
-	if ! grep -E "s DERIVATION|s VERIFIED" -q log/$((i-1)).cubes$c.verify
-	then
-		echo "ERROR: Proof not verified"
-	fi
-	rm $dir/$((i-1)).cubes$c
-	rm "$dir/$((i-1)).cubes$c.drat"
 
 	if [ "$s" == "-m" ]
 	then
@@ -119,6 +109,16 @@ then
 	head $dir/$((i-1)).cubes -n $c | tail -n 1 > $dir/$i-$c.cubes
 	sed 's/a/u/' $dir/$i-$c.cubes -i # Mark cubes that have been shown to be unsatisfiable with 'u'
 	unsat=1
+
+	echo "$dir/$((i-1)).cubes$c is solved by CaDiCaL, thus verifying..."
+	./drat-trim/drat-trim "$dir/$((i-1)).cubes$c" "$dir/$((i-1)).cubes$c.drat" -f | tee log/$((i-1)).cubes$c.verify
+	echo "log/$((i-1)).cubes$c.verify"
+	if ! grep -E "s DERIVATION|s VERIFIED" -q log/$((i-1)).cubes$c.verify
+	then
+		echo "ERROR: Proof not verified"
+	fi
+	rm $dir/$((i-1)).cubes$c
+	rm "$dir/$((i-1)).cubes$c.drat"
 else
 	# Determine how many edge variables were removed
 	removedvars=$(sed -E 's/.* 0 [-]*([0-9]*) 0$/\1/' < $dir/$((i-1)).cubes$c.ext | awk "\$0<=$m" | sort | uniq | wc -l)
